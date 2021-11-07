62970729d5f6aa9d18e8c43f77c7c03c1a896528
Eniola Osabiya <eosabiya@gmail.com>
2021-11-07 21:40:51 +0100

    cleanup of index complete

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
-import Head from "next/head";
 import Link from "next/link";
 
 export default function Home() {
     return (
         <>
-            <Head>
-                <title>Haikoto - Creating a unique workplace culture</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <main className="flex flex-col items-center justify-center w-full flex-1 px-1 lg:px-20 text-center">
                     {/* Reduc text size mobile */}
@@ -20,13 +14,13 @@ export default function Home() {
 
                     {/* Login | Signup Buttons */}
                     <div className="flex items-center justify-center w-full">
-                        <Link href="auth/login">
+                        <Link href="login">
                             <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                                 Login
                             </a>
                         </Link>
                         <span className="mx-2"></span>
-                        <Link href="auth/signup">
+                        <Link href="signup">
                             <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
                                 Signup
                             </a>
