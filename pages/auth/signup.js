50ea79df36b768c6f7ad9169ff57e257e5699099
Eniola Osabiya <eosabiya@gmail.com>
2021-11-07 19:59:05 +0100

    change auth required data to codename

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -31,13 +31,10 @@ export default function Signup() {
                             onSubmit={(e) => e.preventDefault()}
                         >
                             <div className="flex flex-col pt-4">
-                                <label htmlFor="email" className="text-lg">
-                                    Email
-                                </label>
+                                <label className="text-lg">Code Name</label>
                                 <input
-                                    type="email"
-                                    placeholder="your@email.com"
-                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
+                                    type="text"
+                                    className="shadow appearance-none border rounded w-full py-3 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"
                                     required
                                 />
                             </div>
