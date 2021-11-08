c9b09706d5fc6cfcd9f86e36571c7230e967f1e6
Eniola Osabiya <eosabiya@gmail.com>
2021-11-08 14:00:57 +0100

    jwt-decode added to decode auth token

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ export default function Home() {
         <>
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <main className="flex flex-col items-center justify-center w-full flex-1 px-1 lg:px-20 text-center">
-                    {/* Reduc text size mobile */}
                     <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
                         Welcome to <b className="text-blue-600">Haikoto</b>
                     </h1>
--- pages/user/index.js
+++ pages/user/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import jwt_decode from "jwt-decode";
 
 import { withAuth } from "../../app/utils";
 
@@ -7,16 +8,24 @@ function Index() {
 
     React.useEffect(() => {
         setUser(sessionStorage.getItem("auth-token"));
+        console.log(jwt_decode(sessionStorage.getItem("auth-token")));
     }, []);
 
     return (
         <>
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <main className="flex flex-col items-center justify-center w-full flex-1 px-1 lg:px-20 text-center">
-                    {/* Reduc text size mobile */}
-                    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
-                        Welcome to <b className="text-blue-600">{user}</b>
-                    </h1>
+                    <div className="text-4xl md:text-5xl lg:text-6xl font-bold">
+                        <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
+                            Hello <b className="text-blue-600">User</b>
+                        </h1>
+                        <div className="my-5" />
+                        <p className="text-lg md:text-xl lg:text-2xl">
+                            You are logged in!
+                            <br />
+                            Token : {user}
+                        </p>
+                    </div>
                 </main>
             </div>
         </>
