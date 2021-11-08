b9421602b1c57c44efc5d2e6143e2508df5dfc93
Eniola Osabiya <eosabiya@gmail.com>
2021-11-09 00:15:30 +0100

    change create-card action text to "Publish"

--- pages/user/create-card.js
+++ pages/user/create-card.js
@@ -185,7 +185,7 @@ function createCard() {
                                         className="bg-black hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
                                         type="submit"
                                     >
-                                        Create Card
+                                        Publish
                                     </button>
                                 </div>
                             </div>
