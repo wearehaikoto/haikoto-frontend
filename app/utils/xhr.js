0aed412342135d3586806bc7c933a4183f5b58b6
Eniola Osabiya <eosabiya@gmail.com>
2021-11-08 23:44:05 +0100

    bug fix: specified public ENV to work in browser

--- app/utils/xhr.js
+++ app/utils/xhr.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = process.env.BACKEND_URL || "http://localhost:8080";
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
 
 const config = {
     baseURL,
