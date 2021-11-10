6f3e8492e51cb37b946e7513b210c7a5e251d3e3
Eniola Osabiya <eosabiya@gmail.com>
2021-11-10 12:41:23 +0100

    changed typeof window !== "undefined" to React UseEffect, console <div> error fixed

--- app/utils/withoutAuth.js
+++ app/utils/withoutAuth.js
@@ -1,26 +1,28 @@
+import React from "react";
 import { useRouter } from "next/router";
 
 const withoutAuth = (WrappedComponent) => {
     return (props) => {
-        // checks whether we are on client / browser or server.
-        if (typeof window !== "undefined") {
-            const router = useRouter();
+        const [user, setUser] = React.useState(null);
 
+        // Use Router instance definition
+        const router = useRouter();
+
+        React.useEffect(() => {
             // Check if access token is in local storage
             const accessToken = localStorage.getItem("auth-token");
 
-            // If this is no accessToken we just render the component that was passed with all its props
-            if (!accessToken) {
-                return <WrappedComponent {...props} />;
+            // If there is an access token we set the user state.
+            setUser(accessToken);
+
+            // If there is no access token we redirect to "/" page.
+            if (accessToken) {
+                router.replace("/user");
             }
+        }, []);
 
-            // If there is access token we redirect to "/user" page.
-            router.replace("/user");
-            return null;
-        }
-
-        // If we are on server, return null
-        return null;
+        // User is null, return the wrapped component.
+        return !user && <WrappedComponent {...props} />;
     };
 };
 
