f248ff56752fe11c22462d15e516e4ff93636bb2
Eniola Osabiya <eosabiya@gmail.com>
2021-11-12 08:34:22 +0100

    Check if game was completed and if card was answered

--- pages/user/about-me.js
+++ pages/user/about-me.js
@@ -8,53 +8,51 @@ import { currentUser, withAuth } from "../../app/utils";
 import { CardCancelButton, LoadingComponent } from "../../app/components";
 
 function createCard() {
-    const user = currentUser().userData;
-    const router = useRouter();
+  const user = currentUser().userData;
+  const router = useRouter();
 
-    const [loadingState, setLoadingState] = React.useState({
+  const [loadingState, setLoadingState] = React.useState({
+    show: true,
+    text: "Loading...",
+    description: ""
+  });
+  const [allGamesByMe, setAllGamesByMe] = React.useState(null);
+
+  React.useEffect(async () => {
+    const getAllGamesByMe = await gameService.getAllByMe();
+
+    if (getAllGamesByMe.success) {
+      setAllGamesByMe(getAllGamesByMe.data);
+      setLoadingState({ show: false, text: "" });
+    } else {
+      setLoadingState({
         show: true,
-        text: "Loading...",
-        description: ""
-    });
-    const [allGamesByMe, setAllGamesByMe] = React.useState(null);
+        text: `Error: ${getAllGamesByMe.message}.`,
+        description: "Redirecting..."
+      });
 
-    React.useEffect(async () => {
-        const getAllGamesByMe = await gameService.getAllByMe();
+      setTimeout(() => {
+        router.push("/user");
+      }, 2000);
+    }
+  }, []);
 
-        if (getAllGamesByMe.success) {
-            setAllGamesByMe(getAllGamesByMe.data);
-            setLoadingState({ show: false, text: "" });
-        } else {
-            setLoadingState({
-                show: true,
-                text: `Error: ${getAllGamesByMe.message}.`,
-                description: "Redirecting..."
-            });
+  console.log(allGamesByMe);
 
-            setTimeout(() => {
-                router.push("/user");
-            }, 2000);
-        }
-    }, []);
+  return (
+    <>
+      <Head>
+        <title>About {user.codeName} - Haikoto</title>
+      </Head>
 
-    console.log(allGamesByMe);
+      {!loadingState.show ? (
+        <div className="items-center justify-center min-h-screen py-2">
+          <div className="m-10 md:mx-44 h-screen">
+            <div className="mb-4">
+              <h1 className="text-center text-xl md:text-3xl">About Me</h1>
+            </div>
 
-    return (
-        <>
-            <Head>
-                <title>About {user.codeName} - Haikoto</title>
-            </Head>
-
-            {!loadingState.show ? (
-                <div className="items-center justify-center min-h-screen py-2">
-                    <div className="m-10 md:mx-44 h-screen">
-                        <div className="mb-4">
-                            <h1 className="text-center text-xl md:text-3xl">
-                                About Me
-                            </h1>
-                        </div>
-
-                        {/* <div className="mb-10 py-16 h-full">
+            {/* <div className="mb-10 py-16 h-full">
                             <div className="grid grid-cols-6 justify-items-stretch items-end gap-3 h-2/3 md:px-16">
                                 <div className="bg-pink-500 h-[70%]" />
                                 <div className="bg-purple-500 h-[80%]" />
@@ -81,88 +79,72 @@ function createCard() {
                             </div>
                         </div> */}
 
-                        {allGamesByMe.map((game) => {
-                            return (
-                                <div
-                                    key={game._id}
-                                    className="mb-10 py-16"
-                                >
-                                    {/* Game Id */}
-                                    <div className="p-4 overflow-x-auto">
-                                        <h3 className="text-xl">Game ID: {game._id}</h3>
-                                        <table className="table-auto w-full">
-                                            <thead className="bg-blue-600">
-                                                <tr>
-                                                    <th className="px-4 py-2 text-xs text-white text-left">
-                                                        Card ID
-                                                    </th>
-                                                    <th className="px-4 py-2 text-xs text-white text-left">
-                                                        Title
-                                                    </th>
-                                                    <th className="px-4 py-2 text-xs text-white text-left">
-                                                        Your Answer
-                                                    </th>
-                                                    <th className="px-4 py-2 text-xs text-white text-left" />
-                                                </tr>
-                                            </thead>
-                                            <tbody className="text-sm">
-                                                {game.cards.map(
-                                                    (card, index) => {
-                                                        return (
-                                                            <tr key={card._id}>
-                                                                <td className="border px-4 py-2 text-blue-600 border-blue-500 font-medium">
-                                                                    {card._id}
-                                                                </td>
-                                                                <td className="border px-4 py-2 text-blue-600 border-blue-500 font-medium">
-                                                                    {
-                                                                        card.cardTitle
-                                                                    }
-                                                                </td>
-                                                                <td className="border px-4 py-2 border-blue-500 font-medium">
-                                                                    {game
-                                                                        .answers[
-                                                                        index
-                                                                    ] ? (
-                                                                        <span className="text-green-500">
-                                                                            {" "}
-                                                                            True{" "}
-                                                                        </span>
-                                                                    ) : (
-                                                                        <span className="text-red-500">
-                                                                            {" "}
-                                                                            False{" "}
-                                                                        </span>
-                                                                    )}
-                                                                </td>
-                                                                <td className="border px-4 py-2 text-yellow-600 border-blue-500 font-medium">
-                                                                    <Link
-                                                                        href={`/card/${card._id}`}
-                                                                    >
-                                                                        <a>
-                                                                            View
-                                                                            Card
-                                                                        </a>
-                                                                    </Link>
-                                                                </td>
-                                                            </tr>
-                                                        );
-                                                    }
-                                                )}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
-                            );
-                        })}
+            {allGamesByMe.map((game) => {
+              // Check if game was completed
+              if (game.answers.length === 0) return;
 
-                        <CardCancelButton />
-                    </div>
+              return (
+                <div key={game._id} className="mb-10 py-16">
+                  <div className="p-4 overflow-x-auto">
+                    <h3 className="text-xl">Game ID: {game._id}</h3>
+                    <table className="table-auto w-full">
+                      <thead className="bg-blue-600">
+                        <tr>
+                          <th className="px-4 py-2 text-xs text-white text-left">
+                            Card ID
+                          </th>
+                          <th className="px-4 py-2 text-xs text-white text-left">
+                            Title
+                          </th>
+                          <th className="px-4 py-2 text-xs text-white text-left">
+                            Your Answer
+                          </th>
+                          <th className="px-4 py-2 text-xs text-white text-left" />
+                        </tr>
+                      </thead>
+                      <tbody className="text-sm">
+                        {game.cards.map((card, index) => {
+                          // Check if game was completed and if card was answered
+                          if (game.answers[index] === undefined) return;
+
+                          return (
+                            <tr key={card._id}>
+                              <td className="border px-4 py-2 text-blue-600 border-blue-500 font-medium">
+                                {card._id}
+                              </td>
+                              <td className="border px-4 py-2 text-blue-600 border-blue-500 font-medium">
+                                {card.cardTitle}
+                              </td>
+                              <td className="border px-4 py-2 border-blue-500 font-medium">
+                                {game.answers[index] ? (
+                                  <span className="text-green-500"> True </span>
+                                ) : (
+                                  <span className="text-red-500"> False </span>
+                                )}
+                              </td>
+                              <td className="border px-4 py-2 text-yellow-600 border-blue-500 font-medium">
+                                <Link href={`/card/${card._id}`}>
+                                  <a>View Card</a>
+                                </Link>
+                              </td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
                 </div>
-            ) : (
-                <LoadingComponent {...loadingState} />
-            )}
-        </>
-    );
+              );
+            })}
+
+            <CardCancelButton />
+          </div>
+        </div>
+      ) : (
+        <LoadingComponent {...loadingState} />
+      )}
+    </>
+  );
 }
 
 export default withAuth(createCard);
