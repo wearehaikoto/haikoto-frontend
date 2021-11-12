b1f805fbbc53fb53a567f15536c4e22fe3dcecbc
Eniola Osabiya <eosabiya@gmail.com>
2021-11-12 08:52:09 +0100

    SingleCard Component restructure

--- pages/play/index.js
+++ pages/play/index.js
@@ -9,121 +9,118 @@ import { currentUser, withAuth } from "../../app/utils";
 import { CardCancelButton, LoadingComponent } from "../../app/components";
 
 function playCards() {
-    const user = currentUser();
-    const router = useRouter();
+  const user = currentUser();
+  const router = useRouter();
 
-    const [loadingState, setLoadingState] = React.useState({
+  const [loadingState, setLoadingState] = React.useState({
+    show: true,
+    text: "Loading...",
+    description: ""
+  });
+  const [gameId, setGameId] = React.useState(null);
+  const [cards, setCards] = React.useState([]);
+  const [answers, setAnswers] = React.useState([]);
+  const [currentCard, setCurrentCard] = React.useState(1);
+
+  const handleAnswerClick = async (cardId, answer) => {
+    // Push user Answer and save to answer state
+    // answers.push({ card: currentCard, _id: cardId, answer });
+    answers.push(answer);
+    setAnswers(answers);
+
+    // Scroll to top so they can see
+    window.scrollTo(0, 0);
+
+    // Get the result of the game
+    const result = await gameService.addAnswer(gameId, { answer });
+
+    if (result.success) {
+      // Check if current Card is equal to number of Cards
+      if (cards.length > currentCard) {
+        // Update the current Card Number +1 and continue game
+        setCurrentCard(currentCard + 1);
+        return;
+      }
+
+      // Update Loading State
+      setLoadingState({ show: true, text: "Generating result..." });
+
+      setTimeout(() => {
+        router.push(`/play/${gameId}`);
+      }, 2000);
+    }
+  };
+
+  React.useEffect(async () => {
+    // Get Numbers only from propmt
+    // const number_of_cards_to_play = parseInt(
+    //     prompt("How many cards do you want to play?", 3)
+    // );
+    const number_of_cards_to_play = 5;
+
+    // Create a new Game
+    const createGame = await gameService.create({
+      numberOfCards: number_of_cards_to_play
+    });
+
+    if (createGame.success) {
+      setGameId(createGame.data._id);
+      setCards(createGame.data.cards);
+      setLoadingState({ show: false, text: "" });
+    } else {
+      setLoadingState({
         show: true,
-        text: "Loading...",
-        description: ""
-    });
-    const [gameId, setGameId] = React.useState(null);
-    const [cards, setCards] = React.useState([]);
-    const [answers, setAnswers] = React.useState([]);
-    const [currentCard, setCurrentCard] = React.useState(1);
+        text: `Error: ${createGame.message}.`,
+        description: "Redirecting..."
+      });
 
-    const handleAnswerClick = async (cardId, answer) => {
-        // Push user Answer and save to answer state
-        // answers.push({ card: currentCard, _id: cardId, answer });
-        answers.push(answer);
-        setAnswers(answers);
+      setTimeout(() => {
+        router.push("/user");
+      }, 2000);
+    }
+  }, []);
 
-        // Scroll to top so they can see
-        window.scrollTo(0, 0);
+  const reactSwipeableHandler = useSwipeable({
+    onSwipedLeft: () => {
+      handleAnswerClick(cards[currentCard - 1]._id, false);
+    },
+    onSwipedRight: () => {
+      handleAnswerClick(cards[currentCard - 1]._id, true);
+    }
+  });
 
-        // Get the result of the game
-        const result = await gameService.addAnswer(gameId, { answer });
+  return (
+    <>
+      <Head>
+        <title>Play Cards - Haikoto</title>
+      </Head>
 
-        if (result.success) {
-            // Check if current Card is equal to number of Cards
-            if (cards.length > currentCard) {
-                // Update the current Card Number +1 and continue game
-                setCurrentCard(currentCard + 1);
-                return;
-            }
+      {!loadingState.show ? (
+        <div
+          {...reactSwipeableHandler}
+          className="flex flex-col items-center justify-center min-h-screen py-2"
+        >
+          <div className="m-8 md:mx-44">
+            {/*  <div className="border-black border-2 border-dashed mb-10 p-4"> */}
+            {/* <div className="mb-4">
+                <h1 className="text-center text-xl md:text-3xl">
+                    Play Cards
+                </h1>
+            </div> */}
 
-            // Update Loading State
-            setLoadingState({ show: true, text: "Generating result..." });
+            <SingleCard
+              card={cards[currentCard - 1]}
+              handleAnswerClick={handleAnswerClick}
+            />
 
-            setTimeout(() => {
-                router.push(`/play/${gameId}`);
-            }, 2000);
-        }
-    };
-
-    React.useEffect(async () => {
-        // Get Numbers only from propmt
-        // const number_of_cards_to_play = parseInt(
-        //     prompt("How many cards do you want to play?", 3)
-        // );
-        const number_of_cards_to_play = 5;
-
-        // Create a new Game
-        const createGame = await gameService.create({
-            numberOfCards: number_of_cards_to_play
-        });
-
-        if (createGame.success) {
-            setGameId(createGame.data._id);
-            setCards(createGame.data.cards);
-            setLoadingState({ show: false, text: "" });
-        } else {
-            setLoadingState({
-                show: true,
-                text: `Error: ${createGame.message}.`,
-                description: "Redirecting..."
-            });
-
-            setTimeout(() => {
-                router.push("/user");
-            }, 2000);
-        }
-    }, []);
-
-    const reactSwipeableHandler = useSwipeable({
-        onSwipedLeft: () => {
-            handleAnswerClick(cards[currentCard - 1]._id, false);
-        },
-        onSwipedRight: () => {
-            handleAnswerClick(cards[currentCard - 1]._id, true);
-        }
-    });
-
-    return (
-        <>
-            <Head>
-                <title>Play Cards - Haikoto</title>
-            </Head>
-
-            {!loadingState.show ? (
-                <div
-                    {...reactSwipeableHandler}
-                    className="flex flex-col items-center justify-center min-h-screen py-2"
-                >
-                    <div className="m-8 md:mx-44">
-                        {/*  <div className="border-black border-2 border-dashed mb-10 p-4"> */}
-                        {/* <div className="mb-4">
-                            <h1 className="text-center text-xl md:text-3xl">
-                                Play Cards
-                            </h1>
-                        </div> */}
-
-                        <SingleCard
-                            cardId={cards[currentCard - 1]._id}
-                            cardImage={cards[currentCard - 1].cardImage}
-                            cardTitle={cards[currentCard - 1].cardTitle}
-                            cardCategory={cards[currentCard - 1].cardCategory}
-                            handleAnswerClick={handleAnswerClick}
-                        />
-
-                        <CardCancelButton />
-                    </div>
-                </div>
-            ) : (
-                <LoadingComponent {...loadingState} />
-            )}
-        </>
-    );
+            <CardCancelButton />
+          </div>
+        </div>
+      ) : (
+        <LoadingComponent {...loadingState} />
+      )}
+    </>
+  );
 }
 
 export default withAuth(playCards);
--- pages/user/about-me.js
+++ pages/user/about-me.js
@@ -53,31 +53,27 @@ function createCard() {
             </div>
 
             {/* <div className="mb-10 py-16 h-full">
-                            <div className="grid grid-cols-6 justify-items-stretch items-end gap-3 h-2/3 md:px-16">
-                                <div className="bg-pink-500 h-[70%]" />
-                                <div className="bg-purple-500 h-[80%]" />
-                                <div className="bg-green-500 h-[50%]" />
-                                <div className="bg-yellow-500 h-[100%]" />
-                                <div className="bg-blue-500 h-[30%]" />
-                                <div className="bg-indigo-500 h-[60%]" />
-                            </div>
+              <div className="grid grid-cols-6 justify-items-stretch items-end gap-3 h-2/3 md:px-16">
+                <div className="bg-pink-500 h-[70%]" />
+                <div className="bg-purple-500 h-[80%]" />
+                <div className="bg-green-500 h-[50%]" />
+                <div className="bg-yellow-500 h-[100%]" />
+                <div className="bg-blue-500 h-[30%]" />
+                <div className="bg-indigo-500 h-[60%]" />
+              </div>
 
-                            <div className="text-center mt-5 tracking-wider">
-                                <h1 className="text-lg">
-                                    <strong className="text-3xl">humble,</strong>{" "}
-                                    smart,
-                                </h1>
-                                <h1 className="text-lg">
-                                    happy,{" "}
-                                    <strong className="text-3xl">confident,</strong>
-                                </h1>
-                                <h1 className="text-lg">
-                                    <strong className="text-3xl">
-                                        problem solver
-                                    </strong>
-                                </h1>
-                            </div>
-                        </div> */}
+              <div className="text-center mt-5 tracking-wider">
+                <h1 className="text-lg">
+                  <strong className="text-3xl">humble,</strong> smart,
+                </h1>
+                <h1 className="text-lg">
+                  happy, <strong className="text-3xl">confident,</strong>
+                </h1>
+                <h1 className="text-lg">
+                  <strong className="text-3xl">problem solver</strong>
+                </h1>
+              </div>
+            </div> */}
 
             {allGamesByMe.map((game) => {
               // Check if game was completed
