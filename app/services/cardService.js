11931a8eba08ca64926d00dfacfcf98f661146a5
Eniola Osabiya <eosabiya@gmail.com>
2021-11-12 09:51:40 +0100

    Delete Card View - Frontend

--- app/services/cardService.js
+++ app/services/cardService.js
@@ -103,6 +103,32 @@ class cardService {
             };
         }
     }
+
+    async deleteCard(cardId) {
+        try {
+            const response = await $http.delete(`/api/card/${cardId}`);
+
+            if (response.data.success) {
+                return {
+                    success: true,
+                    message: response.data.message,
+                    data: response.data.data
+                };
+            } else {
+                return {
+                    success: false,
+                    message: response.data.message
+                };
+            }
+        } catch (error) {
+            return {
+                success: false,
+                message: error.response
+                    ? error.response.data.message
+                    : error.message
+            };
+        }
+    }
 }
 
 export default new cardService();
