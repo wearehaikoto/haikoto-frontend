1f087aa21a3bd740a21a8b55bc610c3db20aee33
Eniola Osabiya <eosabiya@gmail.com>
2021-12-08 19:39:05 +0100

    show #hashtag title in singleCard

--- app/components/SingleCard/SingleCard.js
+++ app/components/SingleCard/SingleCard.js
@@ -27,7 +27,7 @@ function SingleCard({ card, handleAnswerClick }) {
                                 key={hashtag._id}
                                 className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
                             >
-                                #{hashtag.name}
+                                #{hashtag.title}
                             </span>
                         ))}
                     </p>
@@ -86,7 +86,7 @@ function SingleCard({ card, handleAnswerClick }) {
                                         key={hashtag._id}
                                         className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2"
                                     >
-                                        #{hashtag.name}
+                                        #{hashtag.title}
                                     </span>
                                 ))}
                             </p>
