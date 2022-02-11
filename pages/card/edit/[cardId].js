7f2908fe7ce3b4f307e5e56101cf09a23ce987f9
Eniola Osabiya <eosabiya@gmail.com>
2022-02-11 20:09:41 +0100

    chore: updateCard page converted to new design

--- pages/card/edit/[cardId].js
+++ pages/card/edit/[cardId].js
@@ -9,7 +9,8 @@ import { cardService } from "../../../services";
 import {
     LoadingComponent,
     CardCancelButton,
-    AlertComponent
+    AlertComponent,
+    NavigationBarComponent
 } from "../../../components";
 
 function editCard() {
@@ -124,131 +125,142 @@ function editCard() {
             <Head>
                 <title>Edit Card - Haikoto</title>
             </Head>
+            <div className="relative min-h-screen md:flex">
+                <NavigationBarComponent />
 
-            {!loadingState.show ? (
-                <div className="flex flex-col items-center justify-center min-h-screen">
-                    <div className="m-7 md:mx-44">
-                        <div className="mb-2">
-                            <h1 className="text-center text-xl md:text-3xl">
+                <div className="flex-1 p-10 text-2xl font-bold max-h-screen overflow-y-auto">
+                    {loadingState.show && (
+                        <LoadingComponent {...loadingState} />
+                    )}
+
+                    {!loadingState.show && (
+                        <div className="flex flex-col items-center justify-center">
+                            <section className="my-4 w-full p-5 rounded bg-gray-200 bg-opacity-90">
                                 Edit Card
-                            </h1>
-                        </div>
-                        {alertState.show && <AlertComponent {...alertState} />}
-                        <div className="mt-2 p-4 md:py-16 max-w-lg">
-                            <form onSubmit={processUpdateCard}>
-                                <label htmlFor="upload-button">
-                                    <div className="flex justify-center relative">
-                                        <Image
-                                            src={previewImage || card.image}
-                                            width={500}
-                                            height={500}
-                                        />
-                                        {!previewImage && (
-                                            <div className="absolute w-full py-2.5 bottom-1/3 bg-blue-600 opacity-0 text-white text-xs text-center leading-4 hover:opacity-70">
-                                                Click to upload new
-                                            </div>
-                                        )}
-                                    </div>
-                                </label>
-                                <input
-                                    type="file"
-                                    id="upload-button"
-                                    style={{ display: "none" }}
-                                    onChange={async (e) => {
-                                        // Set the Preview Image
-                                        const file = e.target.files[0];
-                                        const reader = new FileReader();
-                                        reader.onload = (e) => {
-                                            setPreviewImage(e.target.result);
-                                        };
-                                        reader.readAsDataURL(file);
+                            </section>
 
-                                        const image = await uploadImage(file);
-                                        if (image.success) {
-                                            // Set the Form Input State
-                                            setFormInput({
-                                                ...formInput,
-                                                image: image.url
-                                            });
-                                        } else {
-                                            window.scrollTo(0, 0);
-                                            setAlertState({
-                                                show: true,
-                                                message:
-                                                    "Image Upload Failed. Please try again.",
-                                                type: "error"
-                                            });
-                                            setPreviewImage(null);
-                                        }
-                                    }}
-                                />
+                            {alertState.show && (
+                                <AlertComponent {...alertState} />
+                            )}
 
-                                <div className="mt-4 mb-8">
-                                    <h1 className="md:text-3xl text-center">
-                                        Edit Image
-                                    </h1>
-                                    <h1 className="font-bold text-xl md:text-3xl text-center mt-4 md:mt-10">
-                                        Title
-                                    </h1>
+                            <div className="mt-2 md:py-10 max-w-lg">
+                                <form onSubmit={processUpdateCard}>
+                                    <label htmlFor="upload-button">
+                                        <div className="flex justify-center relative">
+                                            <Image
+                                                src={previewImage || card.image}
+                                                width={500}
+                                                height={500}
+                                            />
+                                            {!previewImage && (
+                                                <div className="absolute w-full py-2.5 bottom-1/3 bg-blue-600 opacity-0 text-white text-xs text-center leading-4 hover:opacity-70">
+                                                    Click to upload new
+                                                </div>
+                                            )}
+                                        </div>
+                                    </label>
                                     <input
-                                        className="border-black border-2 my-2 w-full p-2"
-                                        type="text"
-                                        defaultValue={card.title}
-                                        onChange={(e) => {
-                                            setFormInput({
-                                                ...formInput,
-                                                title: e.target.value
-                                            });
-                                        }}
-                                    />
-                                    <h1 className="font-bold text-xl md:text-3xl text-center mt-4 md:mt-10">
-                                        Hashtags (Parent Cards)
-                                    </h1>
-                                    <CreatableSelect
-                                        className="border-black border-2 my-2 w-full"
-                                        isMulti
-                                        defaultValue={card.hashtags.map(
-                                            (h) => ({
-                                                value: h._id,
-                                                label: h.title
-                                            })
-                                        )}
-                                        onChange={(e) => {
-                                            setFormInput({
-                                                ...formInput,
-                                                hashtags: e.map((e) =>
-                                                    e.value.toLowerCase().trim()
-                                                )
-                                            });
-                                        }}
-                                        options={cardsAsHashtags.map(
-                                            (hashtag) => {
-                                                return {
-                                                    value: hashtag._id,
-                                                    label: hashtag.title
-                                                };
-                                            }
-                                        )}
-                                    />
-                                    {/* Submit Button */}
-                                    <div className="flex justify-center mt-8">
-                                        <button
-                                            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                                            type="submit"
-                                        >
-                                            Update
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                                        type="file"
+                                        id="upload-button"
+                                        style={{ display: "none" }}
+                                        onChange={async (e) => {
+                                            // Set the Preview Image
+                                            const file = e.target.files[0];
+                                            const reader = new FileReader();
+                                            reader.onload = (e) => {
+                                                setPreviewImage(
+                                                    e.target.result
+                                                );
+                                            };
+                                            reader.readAsDataURL(file);
 
-                        <CardCancelButton />
-                    </div>
+                                            const image = await uploadImage(
+                                                file
+                                            );
+                                            if (image.success) {
+                                                // Set the Form Input State
+                                                setFormInput({
+                                                    ...formInput,
+                                                    image: image.url
+                                                });
+                                            } else {
+                                                window.scrollTo(0, 0);
+                                                setAlertState({
+                                                    show: true,
+                                                    message:
+                                                        "Image Upload Failed. Please try again.",
+                                                    type: "error"
+                                                });
+                                                setPreviewImage(null);
+                                            }
+                                        }}
+                                    />
+
+                                    <div className="mt-4 mb-8">
+                                        <h1 className="md:text-3xl text-center">
+                                            Edit Image
+                                        </h1>
+                                        <h1 className="font-bold text-xl md:text-3xl text-center mt-4 md:mt-10">
+                                            Title
+                                        </h1>
+                                        <input
+                                            className="border-black border-2 my-2 w-full p-2"
+                                            type="text"
+                                            defaultValue={card.title}
+                                            onChange={(e) => {
+                                                setFormInput({
+                                                    ...formInput,
+                                                    title: e.target.value
+                                                });
+                                            }}
+                                        />
+                                        <h1 className="font-bold text-xl md:text-3xl text-center mt-4 md:mt-10">
+                                            Hashtags (Parent Cards)
+                                        </h1>
+                                        <CreatableSelect
+                                            className="border-black border-2 my-2 w-full"
+                                            isMulti
+                                            defaultValue={card.hashtags.map(
+                                                (h) => ({
+                                                    value: h._id,
+                                                    label: h.title
+                                                })
+                                            )}
+                                            onChange={(e) => {
+                                                setFormInput({
+                                                    ...formInput,
+                                                    hashtags: e.map((e) =>
+                                                        e.value
+                                                            .toLowerCase()
+                                                            .trim()
+                                                    )
+                                                });
+                                            }}
+                                            options={cardsAsHashtags.map(
+                                                (hashtag) => {
+                                                    return {
+                                                        value: hashtag._id,
+                                                        label: hashtag.title
+                                                    };
+                                                }
+                                            )}
+                                        />
+                                        {/* Submit Button */}
+                                        <div className="flex justify-center mt-8">
+                                            <button
+                                                className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                                type="submit"
+                                            >
+                                                Update
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    )}
                 </div>
-            ) : (
-                <LoadingComponent {...loadingState} />
-            )}
+            </div>
         </>
     );
 }
